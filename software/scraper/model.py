059c9530101e8468fe3fc3de640417750778d444
Hujnis <tomas.hujnak@gmail.com>
2025-03-12 08:20:44 +0100

    remove model.py, function moved to scraper.py

--- software/scraper/model.py
+++
@@ -1,8 +0,0 @@
-import os
-from transformers import pipeline
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # cesta k souboru model.py (tedy složka scraper)
-models_dir = os.path.join(BASE_DIR, "models", "facebook-bart-large-mnli")
-
-classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
-classifier.save_pretrained(models_dir)
